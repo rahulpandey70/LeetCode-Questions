ac7e262e1c83ecec72872cd761e4d439ddc1e1d8
rahulpandey70 <rahulpandey9103@gmail.com>
2021-10-27 07:25:35 +0530

    added array solution

---
+++ Array/3Sum.py
@@ -0,0 +1,17 @@
+"""
+    Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]], 
+    such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+    Notice that the solution set must not contain duplicate triplets.
+
+    Example:-
+    Input: nums = [-1,0,1,2,-1,-4]
+    Output: [[-1,-1,2],[-1,0,1]]
+
+    Input: nums = []
+    Output: []
+
+    Input: nums = [0]
+    Output: []
+    
+"""
---
+++ Array/MaximumProductSubarray.py
@@ -0,0 +1,19 @@
+"""
+    Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, 
+    and return the product.
+
+    It is guaranteed that the answer will fit in a 32-bit integer.
+    
+    A subarray is a contiguous subsequence of the array.
+
+    Example:-
+    Input: nums = [2,3,-2,4]
+    Output: 6
+    Explanation: [2,3] has the largest product 6.
+
+    Input: nums = [-2,0,-1]
+    Output: 0
+    Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+"""
+
