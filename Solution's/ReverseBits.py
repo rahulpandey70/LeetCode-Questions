28e0639788670096e9459e173675a6c826cfadc6
rahulpandey70 <rahulpandey9103@gmail.com>
2021-11-03 00:41:33 +0530

    added solution for reverse bits

--- Solution's/ReverseBits.py
+++ Solution's/ReverseBits.py
@@ -15,4 +15,13 @@
 
 """
 
-# TODO
+
+def reverseBits(self, n):
+    result = 0
+
+    for i in range(32):
+        result <<= 1
+        if n & 1 > 0:
+            result += 1
+        n >>= 1
+    return result
