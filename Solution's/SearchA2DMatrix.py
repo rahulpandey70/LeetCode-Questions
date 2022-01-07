0df64c08673720e2d1d8858eaa92d75896eb61a8
rahulpandey70 <rahulpandey9103@gmail.com>
2022-01-07 11:02:29 +0530

    modified search a 2d matrix solution

--- Solution's/SearchA2DMatrix.py
+++ Solution's/SearchA2DMatrix.py
@@ -12,3 +12,32 @@
     Output: false
 
     """ 
+def searchMatrix(self, matrix, target) -> bool:
+    rows = len(matrix)
+    if rows == 0: 
+        return False
+        
+    cols = len(matrix[0])
+        
+    rStart, rEnd = 0, rows-1
+    while rStart < rEnd:
+        mid = rStart + (rEnd - rStart) // 2
+        if matrix[mid][cols-1] < target: 
+            rStart = mid + 1
+        elif matrix[mid][0] > target: 
+            rEnd = mid - 1
+        else:
+            rStart = mid
+            break
+        
+    cStart, cEnd = 0, cols - 1
+    while cStart <= cEnd:
+        mid = cStart + (cEnd - cStart) // 2
+        if matrix[rStart][mid] < target: 
+            cStart = mid + 1
+        elif matrix[rStart][mid] > target: 
+            cEnd = mid - 1
+        else: 
+            return True
+        
+    return False
