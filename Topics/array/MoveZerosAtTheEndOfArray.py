4633bee47ab5da5fc962e8e846280591eed26a4e
rahulpandey70 <rahulpandey9103@gmail.com>
2023-07-26 11:32:31 +0530

    update

--- Topics/array/MoveZerosAtTheEndOfArray.py
+++ Topics/array/MoveZerosAtTheEndOfArray.py
@@ -10,6 +10,8 @@
     Output: [6, 8, 2, 3, 4, 1, 0, 0, 0]
     
     1. If the current element is non-zero, place the element at the next available position in the array. After all elements in the array are processed, fill all remaining indices by 0.
+    
+    2. We can also solve this problem in one scan of the array by modifying Quicksort's partitioning logic. The idea is to use 0 as a pivot element and make one pass of the partition process. The partitioning logic reads all elements and swap every non-pivot element with the first occurrence of the pivot.
 
 """
 
@@ -26,6 +28,30 @@ def moveZeroAtTheEnd(nums):
     # move all zero at the end of the array
     for i in range(k, len(nums)):
         nums[i] = 0
+    
+if __name__ == "__main__":
+    
+    nums = [6, 0, 8, 2, 3, 0, 4, 0, 1]
+    moveZeroAtTheEnd(nums)
+    print(nums)
+    
+    
+# ----------------------------------------------------
+
+def swap(nums, i, j):
+    temp = nums[i]
+    nums[i] = nums[j]
+    nums[j] = temp
+
+def moveZeroAtTheEnd(nums):
+    
+    j = 0
+    
+    # each time we encounter non-zero element, j is incremented and the element is placed before pivot
+    for i in range(len(nums)):
+        if nums[i]: # pivot is zero
+            swap(nums, i, j)
+            j = j + 1
     
 if __name__ == "__main__":
     
